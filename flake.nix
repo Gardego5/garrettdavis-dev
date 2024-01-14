58369e3ee81498eb2bc366de9c82969b0899115c
Garrett Davis <me@garrettdavis.dev>
2024-01-14 10:44:38 -0800

    nix shell 2

--- flake.nix
+++ flake.nix
@@ -1,14 +1,10 @@
 {
   inputs = {
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-    };
-    cargo2nix = {
-      url = "github:cargo2nix/cargo2nix/release-0.11.0";
-      inputs.rust-overlay.follows = "rust-overlay";
-    };
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    cargo2nix.inputs.rust-overlay.follows = "rust-overlay";
     flake-utils.follows = "cargo2nix/flake-utils";
     nixpkgs.follows = "cargo2nix/nixpkgs";
   };
@@ -28,19 +24,22 @@
               "terraform"
             ];
         };
+
         pkgsCross = import nixpkgs {
           inherit system;
           crossSystem.config = "aarch64-unknown-linux-musl";
           overlays = [ cargo2nix.overlays.default ];
         };
+
         rustpkgs = pkgs.rustBuilder.makePackageSet {
           inherit
-          rustVersion
-          rustChannel
-          packageFun;
+            rustVersion
+            rustChannel
+            packageFun;
 
           extraRustComponents = [ "clippy" ];
         };
+
         rustpkgs-lambda = pkgsCross.rustBuilder.makePackageSet {
           inherit
             rustVersion
@@ -53,9 +52,8 @@
         devShell = rustpkgs.workspaceShell {
           packages = with pkgs; [ terraform just tailwindcss ];
         };
-        packages = {
-          default = (rustpkgs-lambda.workspace.garrettdavis-dev { }).bin;
-        };
+
+        packages.default = (rustpkgs-lambda.workspace.garrettdavis-dev { }).bin;
       }
     );
 }
